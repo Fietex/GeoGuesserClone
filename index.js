e1591157e08ef2d504d0ebe57126cef5b9efc27b
Fietex <fiete.becker@gmail.com>
2021-07-05 15:19:51 +0200

    Fast Fertig
    benutzbar kommplett
    ein paar städte fehlen
    +vieleicht auf nur innenstädte beschränken

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ let lngMax;
 let lat;
 let lng;
 let cordinates;
+let madeFirstGuess;
 
 /*
 var places = [
@@ -36,8 +37,70 @@ var citybox = [
 		9.90090844394884,
 		10.060337067735812,
 	],
+	[
+		{ city: "münchen" },
+		48.11306455118332,
+		48.1606677712899,
+		11.529315944564669,
+		11.617546725123365,
+	],
+	[
+		{ city: "nürnberg" },
+		49.43146517335345,
+		49.4648297106399,
+		11.0585692150314,
+		11.106634863766534,
+	],
+	[
+		{ city: "stuttgart" },
+		48.75969865312013,
+		48.79061823042675,
+		9.150745613804588,
+		9.202672454558098,
+	],
+	[
+		{ city: "bremen" },
+		53.055967520347664,
+		53.09715927492991,
+		8.77671502762425,
+		8.860011876316854,
+	],
+	[
+		{ city: "hannover" },
+		52.354640970424406,
+		52.388731003250925,
+		9.704214573572017,
+		9.77437394850467,
+	],
+	[
+		{ city: "bielefeld" },
+		52.00668763847063,
+		52.03252037240648,
+		8.511470155968203,
+		8.551378532978699,
+	],
+	[
+		{ city: "dortmund" },
+		51.49249801249454,
+		51.52745704460354,
+		7.434422471940025,
+		7.49747612371945,
+	],
+	[
+		{ city: "essen" },
+		51.436505677938236,
+		51.467253135184976,
+		6.996321451017971,
+		7.032354175707551,
+	],
+	[
+		{ city: "düsseldorf" },
+		51.19103549037801,
+		51.24830059371374,
+		6.754273542406378,
+		6.800196470244276,
+	],
 ];
-
 function initialize() {
 	getNewLocation();
 }
@@ -52,6 +115,7 @@ function getNewLocation() {
 	lat = Math.random() * (latMax - latMin) + latMin;
 	lng = Math.random() * (lngMax - lngMin) + lngMin;
 	cordinates = { lat: lat, lng: lng };
+	madeFirstGuess = false;
 
 	var streetViewService = new google.maps.StreetViewService();
 	var STREETVIEW_MAX_DISTANCE = 50;
@@ -81,17 +145,25 @@ function getNewLocation() {
 function validate(e) {
 	if (e.code === "Enter") {
 		//checks whether the pressed key is "Enter"
-		var text = e.target.value;
+		if (madeFirstGuess == false) {
+			madeFirstGuess = true;
+			var text = e.target.value;
 
-		if (text.toLowerCase() == currentCity[0].city) {
-			score++;
-			document.getElementById("score").innerHTML = "score: " + score;
-			trueResponse();
-			toggleFalseOrTrueResponseOn();
+			if (text.toLowerCase() == currentCity[0].city) {
+				score++;
+				document.getElementById("score").innerHTML = "score: " + score;
+				trueResponse();
+				toggleFalseOrTrueResponseOn();
+			} else {
+				score = 0;
+				console.log(score);
+				document.getElementById("score").innerHTML = "score: " + score;
+				falseResponse();
+				toggleFalseOrTrueResponseOn();
+			}
 		} else {
-			score = 0;
-			falseResponse();
-			toggleFalseOrTrueResponseOn();
+			e.target.value = "";
+			toggleFalseOrTrueResponseOff();
 		}
 	}
 	//validation of the input...
